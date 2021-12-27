2772b46b451bbc110fc509fa1aafd03053005452
Guillaume <guillaume.cobat@gmx.fr>
2021-12-27 18:01:34 +0100

    Add Music loop

--- Code/src/main/model/BoucleJeu.java
+++ Code/src/main/model/BoucleJeu.java
@@ -130,6 +130,7 @@ public class BoucleJeu implements Runnable{
             this.clip = AudioSystem.getClip();
             clip.open(audioInputStream);
             clip.start();
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
         } catch(Exception ex) {
             System.out.println("Error with playing sound.");
             ex.printStackTrace();
