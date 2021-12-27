b7df8497b0e8ebae02e8dbb5d43951d2a1592683
Sylvain <buche.sylvain@gmail.com>
2021-12-27 13:06:05 +0100

    Ecran de GameOver terminé

---
+++ Code/src/main/util/TransparentJPanel.java
@@ -0,0 +1,9 @@
+package main.util;
+
+import javax.swing.*;
+
+public class TransparentJPanel extends JPanel {
+    public TransparentJPanel(){
+        this.setOpaque(false);
+    }
+}
--- Code/src/main/view/GameOver.java
+++ Code/src/main/view/GameOver.java
@@ -2,6 +2,7 @@ package main.view;
 
 import main.util.CustomFont;
 import main.util.CustomJButton;
+import main.util.TransparentJPanel;
 
 import javax.swing.*;
 import java.awt.*;
@@ -9,20 +10,35 @@ import java.awt.*;
 public class GameOver extends JPanel {
     public static CustomJButton retour;
     public GameOver(FenetrePrincipale principale){
-        this.setLayout(new BorderLayout());
+        this.setLayout(new GridLayout(3,3));
+
+        JPanel button = new JPanel();
+        button.setLayout(new GridBagLayout());
+        button.setOpaque(false);
+
 
         JLabel tomb = new JLabel();
+        tomb.setHorizontalAlignment(JLabel.CENTER);
         tomb.setIcon(new ImageIcon(new ImageIcon("Code/resources/others/tomb.png").getImage().getScaledInstance(256, 256, java.awt.Image.SCALE_SMOOTH)));
         JLabel titre = new JLabel("GAME OVER");
         titre.setFont(CustomFont.customFont50);
         titre.setForeground(Color.red);
+        titre.setHorizontalAlignment(JLabel.CENTER);
         this.setBackground(Color.black);
         retour = new CustomJButton("Retour au menu", principale,null, "Code/resources/others/button_background_large.png", null, null, null);
-        this.add(retour);
+        retour.setOpaque(false);
 
-        this.add(titre, BorderLayout.NORTH);
-        this.add(tomb, BorderLayout.CENTER);
-        this.add(retour, BorderLayout.SOUTH);
+        button.add(retour);
+
+        this.add(new TransparentJPanel());
+        this.add(titre);
+        this.add(new TransparentJPanel());
+        this.add(new TransparentJPanel());
+        this.add(tomb);
+        this.add(new TransparentJPanel());
+        this.add(new TransparentJPanel());
+        this.add(button);
+        this.add(new TransparentJPanel());
 
     }
 }
