a322579f9e3e70fae9e05fb8b01f127f84f7adc3
Sylvain Buche <buche.sylvain@gmail.com>
2022-01-04 14:26:53 +0100

    Suppression de vieux code

--- Code/src/main/util/CustomJTextField.java
+++ Code/src/main/util/CustomJTextField.java
@@ -2,44 +2,6 @@ package main.util;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
-/*
-public class CustomJTextField extends JTextField implements FocusListener {
-
-    private final String hint;
-    private boolean showingHint;
-
-    public CustomJTextField(String hint) {
-        super(hint);
-        this.hint = hint;
-        this.showingHint = true;
-        super.addFocusListener(this);
-        this.setPreferredSize(new Dimension(600, 500));
-    }
-
-    @Override
-    public void focusLost(FocusEvent e) {
-        if (this.getText().isEmpty()) {
-            super.setText(hint);
-            showingHint = true;
-        }
-    }
-
-    @Override
-    public String getText() {
-        return showingHint ? "" : super.getText();
-    }
-
-    @Override
-    public void focusGained(FocusEvent e) {
-        if (this.getText().isEmpty()) {
-            super.setText("");
-            showingHint = false;
-        }
-    }
-}
-*/
 
 public class CustomJTextField extends JTextField {
     public CustomJTextField(String hint) {
