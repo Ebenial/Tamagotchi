0593a11b01f694e6b0d6ee2771f631b243ddbf2a
Sylvain Buche <buche.sylvain@gmail.com>
2022-01-04 14:25:37 +0100

    Mise à jour de l'UI du Timer

--- Code/src/main/util/TimerPanel.java
+++ Code/src/main/util/TimerPanel.java
@@ -1,12 +1,8 @@
 package main.util;
 
-import main.model.BoucleJeu;
 import main.view.FenetrePrincipale;
 
-import java.awt.Dimension;
 import java.awt.GridBagLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.Timer;
@@ -25,33 +21,22 @@ public class TimerPanel extends JPanel {
         setLayout(new GridBagLayout());
         label.setFont(CustomFont.customFont50_PLAIN);
         add(label);
-        timer = new Timer(1000, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                count++;
-                //System.out.println("count : "  + count);
+        timer = new Timer(1000, e -> {
+            count++;
 
-                int sec = (int) count % 60;
-                int min = (int) (count / 60) % 60;
-                int hours = (int) ((count / 60) / 60)%24;
-                int days = (int) (((count / 60) / 60) / 24);
+            int sec = (int) count % 60;
+            int min = (int) (count / 60) % 60;
+            int hours = (int) ((count / 60) / 60)%24;
+            int days = (int) (((count / 60) / 60) / 24);
 
-                String strSec = (sec < 10) ? "0" + sec : Integer.toString(sec);
-                String strmin = (min < 10) ? "0" + min : Integer.toString(min);
-                String strHours = (hours < 10) ? "0" + hours : Integer.toString(hours);
-                String strDays = (days < 10) ? "0" + days : Integer.toString(days);
+            String strSec = (sec < 10) ? "0" + sec : Integer.toString(sec);
+            String strmin = (min < 10) ? "0" + min : Integer.toString(min);
+            String strHours = (hours < 10) ? "0" + hours : Integer.toString(hours);
+            String strDays = (days < 10) ? "0" + days : Integer.toString(days);
 
-                //System.out.println(strHours + ":" + strmin + ":" + strSec);
-
-                label.setText(strDays + ":" + strHours + ":" + strmin + ":" + strSec);
-            }
+            label.setText(strDays + "j | " + strHours + "h | " + strmin + "m | " + strSec+"s");
         });
         timer.setInitialDelay(0);
         timer.start();
     }
-
-    public void setCount(long count) {
-        this.count = count;
-    }
-
 }
